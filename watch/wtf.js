23a16981a02497509890551909a3ad0aa7800f7d
duc29122008 <127207290+duc29122008@users.noreply.github.com>
2024-09-29 09:50:54 +0700

    Update wtf.js

--- watch/wtf.js
+++ watch/wtf.js
@@ -11,13 +11,14 @@
         // Add more preroll URLs as needed
     ];
 
-      var PrerollManager = function(player) {
+  var PrerollManager = function(player) {
         this.player = player;
         this.prerolls = [];
         this.currentPreroll = 0;
         this.prerollCounter = document.createElement('div');
         this.prerollCounter.className = 'preroll-counter';
         this.player.el().appendChild(this.prerollCounter);
+        this.mainAudio = document.getElementById('main-audio');
     };
 
     PrerollManager.prototype.initPrerolls = function() {
@@ -84,4 +85,89 @@
             }
         });
     });
+
+      PrerollManager.prototype.disableMainVideoFeatures = function() {
+        this.player.controlBar.hide();
+        this.player.textTrackDisplay.hide();
+        this.mainAudio.pause();
+        this.mainAudio.currentTime = 0;
+    };
+
+    PrerollManager.prototype.enableMainVideoFeatures = function() {
+        this.player.controlBar.show();
+        this.player.textTrackDisplay.show();
+    };
+
+    PrerollManager.prototype.playNextPreroll = function() {
+        if (this.currentPreroll < this.prerolls.length) {
+            this.player.src({ type: 'video/mp4', src: this.prerolls[this.currentPreroll] });
+            this.player.addClass('vjs-preroll');
+            this.prerollCounter.style.display = 'block';
+            this.disableMainVideoFeatures();
+            this.player.one('loadedmetadata', () => {
+                this.player.muted(false);
+                this.updatePrerollCounter();
+                this.player.play();
+            });
+            this.player.one('ended', () => {
+                this.currentPreroll++;
+                this.playNextPreroll();
+            });
+        } else {
+            this.player.removeClass('vjs-preroll');
+            this.prerollCounter.style.display = 'none';
+            this.enableMainVideoFeatures();
+            this.player.src(this.mainContentSrc);
+            this.player.play();
+        }
+    };
+
+    PrerollManager.prototype.start = function() {
+        this.mainContentSrc = this.player.currentSrc();
+        this.player.pause();
+        this.initPrerolls();
+        this.currentPreroll = 0;
+        this.playNextPreroll();
+    };
+
+    // Register the plugin
+    videojs.registerPlugin('prerollManager', function() {
+        var prerollManager = new PrerollManager(this);
+        
+        this.one('loadedmetadata', function() {
+            prerollManager.start();
+        });
+
+        this.on('timeupdate', function() {
+            if (this.hasClass('vjs-preroll')) {
+                prerollManager.updatePrerollCounter();
+            }
+        });
+
+        // Disable seeking during prerolls
+        this.on('seeking', function() {
+            if (this.hasClass('vjs-preroll')) {
+                this.currentTime(this.currentTime() - 0.1);
+            }
+        });
+
+        // Disable volume controls during prerolls
+        var originalVolume = this.volume;
+        var originalMuted = this.muted;
+
+        this.volume = function(percentAsDecimal) {
+            if (!this.hasClass('vjs-preroll')) {
+                return originalVolume.apply(this, arguments);
+            }
+            return this.volume();
+        };
+
+        this.muted = function(muted) {
+            if (!this.hasClass('vjs-preroll')) {
+                return originalMuted.apply(this, arguments);
+            }
+            return this.muted();
+        };
+    });
+})(window.videojs);
 })(window.videojs);
